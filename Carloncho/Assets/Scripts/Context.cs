61ef5048da085deb061fc4a83b9c5cb2548099e1
sebas <spcolonna@gmail.com>
2023-07-08 16:15:11 -0300

    card animation

--- Carloncho/Assets/Scripts/Context.cs
+++ Carloncho/Assets/Scripts/Context.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Context : MonoBehaviour
 {
+    [SerializeField] CardPileView cardPileView;
     
     void Start()
     {
-        
+        var game = new GamePresenter(cardPileView);
     }
 }
---
+++ Carloncho/Assets/Scripts/Domain/Entities/Card.cs
@@ -0,0 +1,11 @@
+﻿public class Card
+{
+    private int suit;
+    private string cardValue;
+
+    public Card(int suit, string cardValue)
+    {
+        this.suit = suit;
+        this.cardValue = cardValue;
+    }
+}
