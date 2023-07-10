025a94cba1ba8aa3c1f120f7c2ed3bfbf33a4226
sebas <spcolonna@gmail.com>
2023-07-10 11:05:09 -0300

    WIP - PlayerController

---
+++ Carloncho/Assets/Editor/Tests/PlayerControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+public class PlayerControllerTests
+{
+	[Test]
+
+	public void AddPlayerToRoom()
+	{
+        var player = new Player();
+        var expected = new List<Player>() { player };
+		var playerController = new PlayerController();
+
+		playerController.AddPlayer(player);
+        var result = playerController.GetPlayers();
+
+        Assert.AreEqual(expected, result);
+	}
+
+    [Test]
+
+    public void AddAnotherPlayerToRoom()
+    {
+        var player = new Player();
+        var anotherPlayer = new Player();
+        var expected = new List<Player>() { player, anotherPlayer };
+        var playerController = new PlayerController();
+
+        playerController.AddPlayer(player);
+        playerController.AddPlayer(anotherPlayer);
+        var result = playerController.GetPlayers();
+
+        Assert.AreEqual(expected, result);
+    }
+
+    /*[Test]
+
+    public void GetFirstPlayerTurn()
+    {
+        var expectedIndex = 0;
+
+        var players = new List<Player>() { new Player() };
+        var playerController = new PlayerController();
+
+        var result = playerController.GetNextPlayerTurn();
+
+        Assert.AreEqual(expectedIndex, result)
+
+    }*/
+}
+
--- Carloncho/Assets/Scripts/GameScene/Domain/Controllers/PlayerController.cs
+++ Carloncho/Assets/Scripts/GameScene/Domain/Controllers/PlayerController.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 
 public class PlayerController
 {
+    private List<Player> players = new List<Player>();
+
+    public void AddPlayer(Player player) => players.Add(player);
+
     public void GetNextPlayerTurn(Action<int> setPlayerTurn)
     {
         throw new NotImplementedException();
     }
+
+    public List<Player> GetPlayers() => players;
 }
