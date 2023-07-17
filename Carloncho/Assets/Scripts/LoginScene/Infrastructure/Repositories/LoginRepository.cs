d2a4631866944f266287fa42f64071fbd11cc0d5
sebas <spcolonna@gmail.com>
2023-07-17 08:27:48 -0300

    clear code

--- Carloncho/Assets/Scripts/LoginScene/Infrastructure/Repositories/LoginRepository.cs
+++ Carloncho/Assets/Scripts/LoginScene/Infrastructure/Repositories/LoginRepository.cs
@@ -1,26 +1,17 @@
-﻿using Mono.Data.Sqlite;
-using Mono.Data;
-using System.Data;
-using UnityEngine;
+﻿using UnityEngine;
+using static UtilsRepository;
+
 
 public class LoginRepository : IRepository
 {
-    private readonly string dbName = "Carloncho";
-
     public bool Exist(UserLogin user)
     {
         var result = false;
 
-        var connString = SetDataBaseClass.SetDataBase(dbName + ".db");
-        IDbConnection dbConn;
-        IDbCommand dbCommand;
-        IDataReader dataReader;
+        OpenConnection();
 
-        dbConn = new SqliteConnection(connString);
-        dbConn.Open();
-        dbCommand = dbConn.CreateCommand();
+        
         var query = "select email from user where email = \"" + user.name + "\" and password = \"" + user.password + "\" ";
-        Debug.Log(query);
         dbCommand.CommandText = query;
         dataReader = dbCommand.ExecuteReader();
         while (dataReader.Read())
@@ -29,12 +20,6 @@ public class LoginRepository : IRepository
             result = true;
         }
 
-        dataReader.Close();
-        dataReader = null;
-        dbCommand.Dispose();
-        dbCommand = null;
-        dbConn.Close();
-        dbConn = null;
 
         return result;
     }
---
+++ Carloncho/Assets/Scripts/LoginScene/Infrastructure/Repositories/UtilsRepository.cs
@@ -0,0 +1,29 @@
+﻿using Mono.Data.Sqlite;
+using System.Data;
+
+public static class UtilsRepository
+{
+    private static readonly string dbName = "Carloncho";
+    public static IDbCommand dbCommand;
+    public static IDataReader dataReader;
+    private static IDbConnection dbConn;
+
+    public static void OpenConnection()
+    {
+        var connString = SetDataBaseClass.SetDataBase(dbName + ".db");
+        dbConn = new SqliteConnection(connString);
+        dbConn.Open();
+        dbCommand = dbConn.CreateCommand();
+    }
+
+    public static void CloseConnection()
+    {
+        dataReader.Close();
+        dataReader = null;
+        dbCommand.Dispose();
+        dbCommand = null;
+        dbConn.Close();
+        dbConn = null;
+    }
+}
+
