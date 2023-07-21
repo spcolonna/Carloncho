71583be5ab275b02ffef34a8ff541f40690b8560
sebas <spcolonna@gmail.com>
2023-07-21 08:59:53 -0300

    WIP - bet decision

---
+++ Carloncho/Assets/Editor/Tests/BetDecisionUseCaseTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+public class BetDecisionUseCaseTest
+{
+	private List<string> cardValues = new() { "A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+
+    [Test]
+	[TestCase("2", "3")]
+    [TestCase("3", "4")]
+    [TestCase("4", "7")]
+    [TestCase("10", "10")]
+    [TestCase("K", "A")]
+    public void Lose_DecisionMinor(string firstCardValue, string secondCardValue)
+	{
+		var decision = 0;
+		var useCase = new BetDecisionUseCase(cardValues);
+
+		var result = useCase.Execute(firstCardValue, secondCardValue, decision);
+
+		Assert.IsFalse(result);
+	}
+
+    [Test]
+    [TestCase("3", "2")]
+    [TestCase("2", "A")]
+    [TestCase("Q", "J")]
+    [TestCase("J", "8")]
+    [TestCase("A", "K")]
+    public void Win_DecisionMinor(string firstCardValue, string secondCardValue)
+    {
+        var decision = 0;
+        var useCase = new BetDecisionUseCase(cardValues);
+
+        var result = useCase.Execute(firstCardValue, secondCardValue, decision);
+
+        Assert.IsTrue(result);
+    }
+}
+
---
+++ Carloncho/Assets/Scripts/GameScene/Domain/UseCases/BetDecisionUseCase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class BetDecisionUseCase
+{
+    private List<string> cardValues;
+
+    public BetDecisionUseCase(List<string> cardValues)
+    {
+        this.cardValues = cardValues;
+    }
+
+    public bool Execute(string firstCardValue, string secondCardValue, int decision)
+    {
+        var firsCardIndex = cardValues.IndexOf(firstCardValue);
+        var secondCardIndex = cardValues.IndexOf(secondCardValue);
+        if (isBigger(decision))
+            return false;
+
+        if (firstCardValue.Equals("K") && secondCardValue.Equals("A"))
+            return false;
+        if (firstCardValue.Equals("A") && secondCardValue.Equals("K"))
+            return true;
+        return firsCardIndex > secondCardIndex;
+    }
+
+    private static bool isBigger(int decision) => decision == 1;
+}
